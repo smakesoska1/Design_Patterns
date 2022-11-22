bfb4a13b6bee5336a12eb639f629c4a43568668c
smakesoska1 <smakesoska1@etf.unsa.ba>
2022-11-22 16:20:40 +0100

    Initial commit

---
+++ src/main/java/ba/unsa/etf/rpr/builderPattern/Builder.java
@@ -0,0 +1,4 @@
+package ba.unsa.etf.rpr.builderPattern;
+
+public class Builder {
+}
---
+++ src/main/java/ba/unsa/etf/rpr/builderPattern/BuilderMain.java
@@ -0,0 +1,10 @@
+package ba.unsa.etf.rpr.builderPattern;
+
+public class BuilderMain {
+
+        public static void main( String[] args )
+        {
+            System.out.println( "Hello World!" );
+        }
+
+}
